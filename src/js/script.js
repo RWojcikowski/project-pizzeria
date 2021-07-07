7c986acda2848dcd11ae362a02c7b8afdfc04579
RWojcikowski <Radoslaw.chicqen@gmail.com>
2021-07-07 16:21:58 +0200

    zadanie 8.1 product quantity widget

--- src/js/script.js
+++ src/js/script.js
@@ -63,12 +63,16 @@
       thisProduct.id = id;
       thisProduct.data = data;
 
+
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
-      console.log(thisProduct);
+
+
+      //console.log(thisProduct);
     }
     renderInMenu() {
       const thisProduct = this;
@@ -95,6 +99,9 @@
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
+
+
     }
     initAccordion() {
       const thisProduct = this;
@@ -107,7 +114,7 @@
       /* START: add event listener to clickable trigger on event click */
 
       clickableTrigger.addEventListener('click', function (event) {
-        console.log(clickableTrigger);
+        // console.log(clickableTrigger);
         /* prevent default action for event */
 
         event.preventDefault();
@@ -121,7 +128,7 @@
             product.classList.remove('active');
             /* toggle active class on thisProduct.element */
             // thisProduct.element.classList.toggle('active');
-            console.log(thisProduct);
+            //console.log(thisProduct);
           }
         });
       });
@@ -130,7 +137,7 @@
 
     initOrderForm() {
       const thisProduct = this;
-      console.log(thisProduct);
+      //console.log(thisProduct);
 
       thisProduct.form.addEventListener('submit', function (event) {
         event.preventDefault();
@@ -154,7 +161,7 @@
 
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
+      // console.log('formData', formData);
 
       // set price to default price
       let price = thisProduct.data.price;
@@ -163,13 +170,13 @@
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
+        //console.log(paramId, param);
 
         // for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+          //console.log(optionId, option);
           const optionSelected =
             formData[paramId] &&
             formData[paramId].includes(optionId);
@@ -195,13 +202,89 @@
         }
       }
 
-      // update calculated price in the HTML
+      /*update calculated price in the HTML*/
+      price *= thisProduct.amountWidget.value;
+
       thisProduct.priceElem.innerHTML = price;
+    }
+    initAmountWidget() {
+      const thisProduct = this;
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+
+      thisProduct.amountWidgetElem.addEventListener('update', function () {
+        thisProduct.processOrder();
+      });
     }
 
 
-
   }
+
+  class AmountWidget {
+    constructor(element) {
+      const thisWidget = this;
+      // console.log('AmountWidget', thisWidget);
+      // console.log('constructor argument:', element);
+
+      thisWidget.getElements(element);
+      thisWidget.value = thisWidget.getElements(element);
+      thisWidget.setValue(settings.amountWidget.defaultValue);
+      thisWidget.initActions();
+    }
+
+
+    getElements(element) {
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+
+    }
+
+    setValue(value) {
+      const thisWidget = this;
+      /* TODO: Add validation */
+      const newValue = parseInt(value);
+      thisWidget.input.value = thisWidget.value;
+
+      if (
+        newValue !== thisWidget.value &&
+        newValue >= settings.amountWidget.defaultMin &&
+        newValue <= settings.amountWidget.defaultMax
+      )
+        thisWidget.value = newValue;
+      thisWidget.announce();
+
+
+      thisWidget.input.value = thisWidget.value;
+    }
+
+    initActions() {
+      const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', function () {
+      });
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.linkDecrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+      thisWidget.linkIncrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+    }
+    announce() {
+      const thisWidget = this;
+
+      const event = new Event('update');
+      thisWidget.element.dispatchEvent(event);
+    }
+  }
+
+
+
   const app = {
 
 
@@ -245,3 +328,6 @@
   };
   app.init();
 }
+
+
+
