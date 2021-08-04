b2c87f35de932d959cc97158dc52ae98f4a64d2f
RWojcikowski <Radoslaw.chicqen@gmail.com>
2021-08-04 16:44:03 +0200

    add pages and booking site

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,64 @@
-import { settings, select, classNames, templates } from './settings.js';
+/* eslint-disable no-unused-vars */
+import { settings, select, classNames, templates, } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+    debugger;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    thisApp.activatePage(thisApp.pages[0].id);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = thisApp;
+        event.preventDefault();
+        /* */
+        const id = clickedElement.getAtribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        /*change url hash  */
+        window.location.hash = '#/' + id;
+
+      });
+    }
+
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+    /*add class "active" to matching pages, remote from non-matching*/
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /*add class "active" to matching links, remote from non-matching*/
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAtribute('href') == '#' + pageId);
+    }
+
+  },
+
   initMenu: function () {
     const thisApp = this;
     // console.log('thisApp.data', thisApp.data);
@@ -28,7 +84,7 @@ const app = {
     const thisApp = this;
     thisApp.data = {};
     // thisApp.data = dataSource;
-    const url = settings.db.url + '/' + settings.db.product;
+    const url = settings.db.url + '/' + settings.db.products;
 
     fetch(url)
 
@@ -52,8 +108,11 @@ const app = {
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
+
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+
   }
 };
 app.init();
